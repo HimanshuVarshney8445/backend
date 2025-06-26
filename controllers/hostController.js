cb28792c9e6cd2742fd502d400317c73c263cd3c
HimanshuVarshney8445 <himanshu897946@gmail.com>
2025-06-26 18:07:02 +0530

    adding marks table

--- controllers/hostController.js
+++ controllers/hostController.js
@@ -4,8 +4,10 @@ exports.addResult = (req,res,next)=>{
 
 exports.addPostResult = (req,res,next)=>{
     const{className,students}=req.body;
+    const subjects = req.body.subjectList.split(',').map(s => s.trim());
     console.log("Class:",className);
     console.log("No. of students:",students);
-    res.redirect("/");
+    res.render("host/table",{pageTitle: "Result Table",studentCount: students,subjects: subjects, className: className});
 }
-    
+
+
--- routers/hostRouter.js
+++ routers/hostRouter.js
@@ -3,9 +3,13 @@ const hostRouter = express.Router();
 
 const {addResult} = require('../controllers/hostController');
 const {addPostResult} = require('../controllers/hostController');
+// const {submitMarks} = require('../controllers/hostController');
+// const {postSubmitMarks} = require('../controllers/hostController');
 
 
 hostRouter.get("/add-result",addResult);
 hostRouter.post("/add-result",addPostResult);
+// hostRouter.get("/submit-marks",addPostResult);
+// hostRouter.post("/submit-marks",postSubmitMarks);
 
 module.exports = hostRouter;
