9c36487ff89215aae676c05e3cf658abeb8107c5
HimanshuVarshney8445 <himanshu897946@gmail.com>
2025-07-14 11:48:02 +0530

    optimize

--- routers/storeRouter.js
+++ routers/storeRouter.js
@@ -3,9 +3,12 @@ const userRouter = express.Router();
 
 const {dashboard} = require('../controllers/userController');
 const {viewResult} = require('../controllers/userController');
+const {profile} = require('../controllers/userController');
 const {protect} = require("../middleware/validation");
+const user = require('../models/user');
 
 userRouter.get("/",dashboard);
+userRouter.get("/profile",protect,profile);
 userRouter.get("/view-result",protect,viewResult);
 
 module.exports = userRouter;
