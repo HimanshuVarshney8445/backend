e64882b6f475ea0b02bf35f4b4b697d44ae09394
HimanshuVarshney8445 <himanshu897946@gmail.com>
2025-07-14 10:59:59 +0530

    optimize

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.dashboard = (req,res,next)=>{
     const authent = req.cookies.token;
     res.render('store/home',{pageTitle: "Dashboard",authentication: authent});
@@ -6,4 +8,17 @@ exports.dashboard = (req,res,next)=>{
 exports.viewResult = (req, res, next)=>{
     const authent = req.cookies.token;
     res.render('store/viewResult', {pageTitle: "View Result",authentication: authent});
+}
+
+exports.profile = async (req, res, next)=>{
+    const authent = req.cookies.token;
+    const user = await User.findById(req.user._id);
+    try{
+        if(!user){
+            throw new Error("User not found");
+        }
+    }catch(err){
+        return next(err);
+    }
+    res.render('store/profile', {pageTitle: "Profile",authentication: authent, user: user});
 }
