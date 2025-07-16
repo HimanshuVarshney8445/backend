9407356e510096e4e66b93af07c3382ef9252861
HimanshuVarshney8445 <himanshu897946@gmail.com>
2025-07-16 14:26:54 +0530

    optimize

--- controllers/hostController.js
+++ controllers/hostController.js
@@ -1,9 +1,9 @@
-exports.addResult = (req,res,next)=>{
+exports.addBatch = async (req,res,next)=>{
     const authent = req.cookies.token;
     res.render('host/add-result',{pageTitle: "Add Result",authentication: authent});
 }
 
-exports.addPostResult = (req,res,next)=>{
+exports.addPostBatch = (req,res,next)=>{
     const{className,students}=req.body;
     const subjects = req.body.subjectList.split(',').map(s => s.trim());
     console.log("Class:",className);
