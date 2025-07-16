f91d7496a971d73db1f2a38983979b87830b30c6
HimanshuVarshney8445 <himanshu897946@gmail.com>
2025-07-16 14:31:42 +0530

    changes

--- routers/storeRouter.js
+++ routers/storeRouter.js
@@ -4,11 +4,13 @@ const userRouter = express.Router();
 const {dashboard} = require('../controllers/userController');
 const {viewResult} = require('../controllers/userController');
 const {profile} = require('../controllers/userController');
+const {update} = require('../controllers/userController');
 const {protect} = require("../middleware/validation");
-const user = require('../models/user');
+// const user = require('../models/user');
 
 userRouter.get("/",dashboard);
 userRouter.get("/profile",protect,profile);
 userRouter.get("/view-result",protect,viewResult);
+userRouter.post("/profile-update/:id",update);
 
 module.exports = userRouter;
